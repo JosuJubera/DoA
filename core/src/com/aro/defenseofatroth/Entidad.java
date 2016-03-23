f17e846dc5b3460204bd434e8f3c8ae242a106fb
Serarmen <Sergio@PORTATIL>
2016-03-23 15:44:52 +0100

    Creo k ya esta

--- core/src/com/aro/defenseofatroth/Entidad.java
+++ core/src/com/aro/defenseofatroth/Entidad.java
@@ -5,24 +5,27 @@ import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.utils.Disposable;
 
 /**
- * Esta clase representa a toda cosa que sea dibujable
+ * Esta clase representa a toda cosa que sea dibujable. En el futuro es posible que herede de Actor
+ * y se usen las Actions, pero ya veremos
  * Created by Sergio on 22/03/2016.
  */
-public abstract class Entidad{
+public abstract class Entidad implements Disposable{
 
     public Sprite texture; //guarda la textura sin mas
     public Animation animacion; //guarda la animacion si tiene
-    public Vector2 posicion; //posicion del objeto
-    public Vector2 velocidad; //velocidad del objeto
-    public Vector2 aceleracion; //aceleracion del objeto
-    public
+    protected Vector2 posicion; //posicion del objeto
+    protected Vector2 velocidad; //velocidad del objeto
+    protected Vector2 aceleracion; //aceleracion del objeto
 
     Entidad(){
         texture=null;
         animacion=null;
+        posicion=new Vector2(0f,0f);
+        velocidad=new Vector2(0f,0f);
+        aceleracion=new Vector2(0f,0f);
     }
 
     public void setTexture(TextureRegion text){
@@ -32,7 +35,29 @@ public abstract class Entidad{
     /**
      *  Dibuja la entidad en el batch. Este se ha de llamar dentro de batch.begin()
      * @param batch donde dibujar
-     * @param parentAlpha tiempo
+     * @param delta tiempo
      */
-    public  abstract void draw(Batch batch,float parentAlpha);
+    public  abstract void draw(Batch batch,float delta);
+
+    public void setAceleracion(Vector2 acel){
+        this.aceleracion=acel;
+    }
+
+    public Vector2 getAceleracion() {
+        return aceleracion;
+    }
+
+    public void setVelocidad(Vector2 velocidad){
+        this.velocidad=velocidad;
+    }
+    public Vector2 getVelocidad(){
+        return this.velocidad;
+    }
+    public void setPosicion(Vector2 posicion){
+        this.posicion=posicion;
+    }
+
+    public Vector2 getPosicion(){
+        return this.posicion;
+    }
 }
--- core/src/com/aro/defenseofatroth/Unidad.java
+++ core/src/com/aro/defenseofatroth/Unidad.java
@@ -1,6 +1,10 @@
 package com.aro.defenseofatroth;
 
-import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.NinePatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 /**
@@ -9,5 +13,30 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 public class Unidad extends Entidad {
     private int vidaMaxima;
     private int vidaActual;
+    private NinePatch barraVidaFondo;
+    private NinePatch barraVidaDelante;
 
+    TextureRegion broja;
+    private float animationTime=0;
+    public Unidad(){
+        //new NinePatch(new TextureRegion(texture, x, y, width, height), left, right, top, bottom);
+        // De la roja: ancho: 36 47 alto 39 47
+        broja=new TextureRegion(new Texture(Gdx.files.internal("barraRoja.png")));
+        barraVidaFondo=new NinePatch(broja, 35, 35, 39, 39); //es una prueba
+    }
+
+    @Override
+    public void draw(Batch batch, float delta) {
+        animationTime+=delta;
+        TextureRegion animaconFrame = animacion.getKeyFrame(animationTime);
+        batch.draw(animaconFrame,posicion.x,posicion.y);
+        barraVidaFondo.draw(batch,0,0,300,30);
+        //batch.draw(broja,0,0);
+
+    }
+
+    @Override
+    public void dispose() {
+        //Si hay k limpiar algo, aqui ira
+    }
 }
