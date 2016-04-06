54321557aed1be8e4839a6f87a7fe63b1d908d35
Jub3r <josujubera@hotmail.com>
2016-04-06 16:28:31 +0200

    Nuevo splashscreen

--- core/src/com/aro/defenseofatroth/Screens/SplashScreen.java
+++ core/src/com/aro/defenseofatroth/Screens/SplashScreen.java
@@ -133,6 +133,8 @@ public class SplashScreen extends BaseScreen {
     private Stage stage;
     private Skin skin;
     private ProgressBar progressBar;
+    private Texture logo;
+    private SpriteBatch batch;
 
     public SplashScreen(MainClass game) {
         super(game);
@@ -140,21 +142,31 @@ public class SplashScreen extends BaseScreen {
         stage = new Stage(new FitViewport(VIRTUAL_WIDTH, VIRTUAL_HEIGHT));
         skin = new Skin(Gdx.files.internal("data/uiskin.json"));
 
-        Table table = new Table(skin);
-        table.setFillParent(true);
+//        Table table = new Table(skin);
+//        table.setFillParent(true);
+
+        batch = new SpriteBatch();
+        logo = new Texture(Gdx.files.internal("logo.png"));
 
         progressBar = new ProgressBar(0, 100, 1, false, skin);
 //        progressBar.setAnimateDuration(.75f);
 
-        table.add(progressBar).size(500, 10);
 
-        stage.addActor(table);
+//        table.add(progressBar).size(500, 10);
+
+        stage.addActor(progressBar);
+        progressBar.setPosition(VIRTUAL_WIDTH /2 - VIRTUAL_WIDTH /8 , VIRTUAL_HEIGHT * 2 /8);
+        progressBar.setSize(VIRTUAL_WIDTH / 3 , 1);
     }
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.begin();
+        batch.draw(logo, 0, Gdx.graphics.getHeight() / 2, Gdx.graphics.getWidth(), Gdx.graphics.getHeight()/2);
+        batch.end();
 
         if (game.getManager().update()) {
             game.finishLoading();
