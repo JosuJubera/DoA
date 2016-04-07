2d54945246ae9d032179ea3eab2926a8e93ba593
Jub3r <josujubera@hotmail.com>
2016-04-07 20:15:46 +0200

    Hud puesto valores

--- core/src/com/aro/defenseofatroth/Screens/Hud.java
+++ core/src/com/aro/defenseofatroth/Screens/Hud.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.Disposable;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
@@ -18,7 +19,7 @@ import static com.badlogic.gdx.graphics.Color.BLACK;
 /**
  * Created by elementary on 7/04/16.
  */
-public class Hud {
+public class Hud implements Disposable {
 
     public Stage stage;
     private Viewport viewport;
@@ -75,8 +76,13 @@ public class Hud {
         table.add(moneyLabel).expandX();
 
         stage.addActor(table);
-
     }
+
+    @Override
+    public void dispose() {
+        stage.dispose();
+    }
+
 
     public static void addScore(int value) {
         score += value;
