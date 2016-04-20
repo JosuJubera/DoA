ec9e82cdf444eb7f001beca61b88f18a156622a4
Serarmen <Sergio@PORTATIL>
2016-04-20 17:27:50 +0200

    Implementando el control de colisiones, falta por acabar

--- core/src/com/aro/defenseofatroth/Game/CollisionControl.java
+++ core/src/com/aro/defenseofatroth/Game/CollisionControl.java
@@ -3,6 +3,7 @@ package com.aro.defenseofatroth.Game;
 import com.badlogic.gdx.physics.box2d.Contact;
 import com.badlogic.gdx.physics.box2d.ContactImpulse;
 import com.badlogic.gdx.physics.box2d.ContactListener;
+import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.Manifold;
 
 /**
@@ -13,6 +14,14 @@ import com.badlogic.gdx.physics.box2d.Manifold;
 public class CollisionControl implements ContactListener {
     @Override
     public void beginContact(Contact contact) {
+        //Cojemos la fixture de los implicados
+        Fixture fixtureA = contact.getFixtureA();
+        Fixture fixtureB = contact.getFixtureB();
+        //Comprobamos que choca, si es un sensor o un proyectil
+        //comprobamos si son enemigo y sensor de torre
+        if (isInRange(fixtureA,fixtureB)){
+            //Obtenemos la torre y le decimos que ataque.
+        }
 
     }
 
@@ -29,5 +38,9 @@ public class CollisionControl implements ContactListener {
     @Override
     public void postSolve(Contact contact, ContactImpulse impulse) {
 
+    }
+    //Devuelve si la colision es de una torre y un enemmigo
+    private boolean isInRange(Fixture objectA,Fixture objectB){
+       return (((objectA.getUserData() instanceof Enemy)&&(objectB.getUserData() instanceof Tower))||((objectB.getUserData() instanceof Enemy)&&(objectA.getUserData() instanceof Tower)));
     }
 }
